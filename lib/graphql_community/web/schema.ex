00865d477d14cdccd1b3a0b3908ef813fbdd9640
Philip Alekseev <phil.alekseev@gmail.com>
2017-08-19 20:54:11 -0400

    Define query

---
+++ lib/graphql_community/web/schema.ex
@@ -0,0 +1,15 @@
+defmodule GraphqlCommunity.Web.Schema do
+  use Absinthe.Schema
+
+  alias GraphqlCommunity.{Web, News}
+
+  object :link do
+    field :id, non_null(:id)
+    field :url, non_null(:string)
+    field :description, non_null(:string)
+  end
+
+  query do
+    field :all_links, non_null(list_of(non_null(:link)))
+  end
+end
